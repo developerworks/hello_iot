bfa121339621fa3918ac91b7e65900b63f94e0b8
developerworks <developerworks@163.com>
2016-12-07 01:40:04 +0800

    添加网络设置

--- apps/fw/config/config.exs
+++ apps/fw/config/config.exs
@@ -24,3 +24,27 @@ config :logger, :console,
   handle_sasl_reports: true,
   handle_otp_reports: true,
   utc_log: true
+
+# ------------
+# 网络配置
+# ------------
+# 产品环境中, 需要提供一个Web界面让用户设置WIFI连接信息,
+# 并且该设置信息需要存储到持久存储中, 比如SQLITE数据库或配置文件.
+
+config :fw, :eth0,
+  opts: [mode: "dhcp"]
+
+config :fw, :wlan0,
+  ssid: "gx888888",
+  key_mgmt: :"WPA-PSK",
+  psk: "gx888888"
+
+config :fw, :wlan0_static,
+  ipv4_address_method: :static,
+  ipv4_address: "192.168.0.200",
+  ipv4_subnet_mask: "255.255.255.0",
+  domain: "www.totoro-game.com",
+  nameservers:  ["192.168.0.1", "8.8.8.8", "8.8.4.4", "114.114.114.114"],
+  ssid: "gx888888",
+  key_mgmt: "WPA-PSK",
+  psk: "gx888888"
