b4125293814f49bbb8ccc1f67725800e463a06ce
developerworks <developerworks@163.com>
2016-12-07 01:37:20 +0800

    添加 Phoenix 端点配置

--- apps/fw/config/config.exs
+++ apps/fw/config/config.exs
@@ -1,1 +1,14 @@
 use Mix.Config
+
+# ------------
+# Phoenix 端点
+# ------------
+config :ui, Ui.Endpoint,
+  http: [port: 80],
+  url: [host: "0.0.0.0", port: 80],
+  secret_key_base: "9w9MI64d1L8mjw+tzTmS3qgJTJqYNGJ1dNfn4S/Zm6BbKAmo2vAyVW7CgfI3CpII",
+  root: Path.dirname(__DIR__),
+  server: true,
+  render_errors: [accepts: ~w(html json)],
+  pubsub: [name: Ui.PubSub,
+           adapter: Phoenix.PubSub.PG2]
