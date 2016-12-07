ab22ec68a9a691ea91acefba34943941ea9cf21c
developerworks <developerworks@163.com>
2016-12-07 15:49:33 +0800

    配置以太网络, 删除注释

--- apps/fw/lib/fw.ex
+++ apps/fw/lib/fw.ex
@@ -1,20 +1,26 @@
 defmodule Fw do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
+  @interface :eth0
+  # @opts [mode: "static", ip: "10.0.10.3", mask: "16", subnet: "255.255.0.0"]
+  @opts [mode: "dhcp"]
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
     children = [
-      # worker(Fw.Worker, [arg1, arg2, arg3]),
+      supervisor(Phoenix.PubSub.PG2, [Nerves.PubSub, [poolsize: 1]]),
+      worker(Task, [fn -> start_network end], restart: :transient)
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Fw.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def start_network do
+    Nerves.Networking.setup @interface, @opts
+    opts = Application.get_env(:fw, :wlan0)
+    Nerves.InterimWiFi.setup "wlan0", opts
   end
 
 end
