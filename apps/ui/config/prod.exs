aa408faec70fb01e936dd831c3578c7d8386f353
developerworks <developerworks@163.com>
2016-12-07 17:21:13 +0800

    80端口被固件升级占用, 改用8080

--- apps/ui/config/prod.exs
+++ apps/ui/config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :ui, Ui.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "0.0.0.0", port: 80],
+  url: [host: "0.0.0.0", port: 8080],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
