7f70d94a79e539fad169c67bc1bc6a0268a0465c
developerworks <developerworks@163.com>
2016-12-13 19:04:42 +0800

    Fix errors

--- apps/ui/mix.exs
+++ apps/ui/mix.exs
@@ -5,7 +5,7 @@ defmodule Ui.Mixfile do
     [app: :ui,
      version: "0.0.1",
      build_path: "../../_build",
-     config_path: "../../config/config.exs",
+     # config_path: "../../config/config.exs",
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
      elixir: "~> 1.2",
